8ff0565e55758f3385044b8e1d06fa91ff4333d4
Guga Todua <gugatodua@gmail.com>
2018-10-19 01:34:33 +0400

    initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace GuramAppWPF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.ADD = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox7 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.button1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.button1.Location = new System.Drawing.Point(24, 286);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(313, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ძებნა";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ItemHeight = 13;
+            this.comboBox1.Location = new System.Drawing.Point(92, 17);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(202, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.ItemHeight = 13;
+            this.comboBox2.Location = new System.Drawing.Point(92, 66);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(202, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(449, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(329, 530);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // ADD
+            // 
+            this.ADD.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.ADD.Location = new System.Drawing.Point(24, 341);
+            this.ADD.Name = "ADD";
+            this.ADD.Size = new System.Drawing.Size(313, 56);
+            this.ADD.TabIndex = 6;
+            this.ADD.Text = "დამატება";
+            this.ADD.UseVisualStyleBackColor = true;
+            this.ADD.Click += new System.EventHandler(this.ADD_Click);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.ItemHeight = 13;
+            this.comboBox3.Location = new System.Drawing.Point(92, 115);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(202, 21);
+            this.comboBox3.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "ავტორი";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "ჟანრი";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.ItemHeight = 13;
+            this.comboBox4.Location = new System.Drawing.Point(92, 165);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(202, 21);
+            this.comboBox4.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "ქვეყანა";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "წიგნი";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button2.Location = new System.Drawing.Point(24, 403);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(313, 56);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "გასუფთავება";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "თარიღი";
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.ItemHeight = 13;
+            this.comboBox5.Location = new System.Drawing.Point(92, 247);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(76, 21);
+            this.comboBox5.TabIndex = 15;
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.ItemHeight = 13;
+            this.comboBox6.Location = new System.Drawing.Point(203, 247);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(76, 21);
+            this.comboBox6.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label6.Location = new System.Drawing.Point(176, 238);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 29);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "-";
+            // 
+            // comboBox7
+            // 
+            this.comboBox7.FormattingEnabled = true;
+            this.comboBox7.ItemHeight = 13;
+            this.comboBox7.Location = new System.Drawing.Point(92, 214);
+            this.comboBox7.Name = "comboBox7";
+            this.comboBox7.Size = new System.Drawing.Size(202, 21);
+            this.comboBox7.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(23, 217);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "წელი";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 589);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.ADD);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button ADD;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox7;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,423 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Infrastructure;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuramAppWPF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            using (var _ctx = new GuramDBEntities())
+            {
+
+                _ctx.Database.CommandTimeout = 200;
+                comboBox1.DataSource = _ctx.Authors.Select(y => y.Name).ToList();
+                comboBox1.SelectedIndex = -1;
+            }
+
+
+            using (var _ctx = new GuramDBEntities())
+            {
+                _ctx.Database.CommandTimeout = 200;
+                comboBox2.DataSource = _ctx.Books.Select(y => y.Name).ToList();
+                comboBox2.SelectedIndex = -1;
+            }
+
+
+            using (var _ctx = new GuramDBEntities())
+            {
+                _ctx.Database.CommandTimeout = 200;
+                comboBox3.DataSource = _ctx.Genres.Select(y => y.Name).ToList();
+                comboBox3.SelectedIndex = -1;
+            }
+
+
+            using (var _ctx = new GuramDBEntities())
+            {
+                _ctx.Database.CommandTimeout = 200;
+                comboBox4.DataSource = _ctx.Countries.Select(y => y.Name).ToList();
+                comboBox4.SelectedIndex = -1;
+            }
+
+            //comboBox7.Enabled = false;
+            //comboBox6.Enabled = false;
+            //comboBox5.Enabled = false;
+
+        }
+
+        
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var name = comboBox1.Text;
+            var book = comboBox2.Text;
+            var genre = comboBox3.Text;
+            var country = comboBox4.Text;
+            var inputYear = comboBox7.Text;
+            var inputStartDate = comboBox5.Text;
+            var inputEndDate = comboBox6.Text;
+
+            int yearResult = 0;
+            int? year = null;
+
+            if (comboBox7.Text == string.Empty)
+            {
+                year = null;
+            }
+            else
+            {
+                if (Int32.TryParse(comboBox7.Text, out yearResult))
+                {
+                    year = yearResult;
+                }
+                else throw new Exception("გთხოვთ შეიყვანოთ სწორი წელი");
+            }
+
+
+
+            int startDateResult = 0;
+            int? startDate = null;
+
+            if (inputStartDate == string.Empty)
+            {
+                startDate = null;
+            }
+            else
+            {
+                if (Int32.TryParse(inputStartDate, out startDateResult))
+                {
+                    startDate = startDateResult;
+                }
+                else throw new Exception("გთხოვთ შეიყვანოთ სწორი წელი");
+            }
+
+
+
+            int endDateResult = 0;
+            int? endDate = null;
+
+            if (inputEndDate == string.Empty)
+            {
+                endDate = null;
+            }
+            else
+            {
+                if (Int32.TryParse(inputEndDate, out endDateResult))
+                {
+                    endDate = endDateResult;
+                }
+                else throw new Exception("გთხოვთ შეიყვანოთ სწორი წელი");
+            }
+
+            #region
+            //void Disabler()
+            //{
+            //    if (comboBox1.Text == string.Empty)
+            //    {
+            //        comboBox1.Enabled = false;
+            //    }
+            //    if (comboBox2.Text == string.Empty)
+            //    {
+            //        comboBox2.Enabled = false;
+            //    }
+            //    if (comboBox2.Text == string.Empty)
+            //    {
+            //        comboBox2.Enabled = false;
+            //    }
+            //    if (comboBox3.Text == string.Empty)
+            //    {
+            //        comboBox3.Enabled = false;
+            //    }
+            //    if (comboBox4.Text == string.Empty)
+            //    {
+            //        comboBox4.Enabled = false;
+            //    }
+            //    if (comboBox5.Text == string.Empty)
+            //    {
+            //        comboBox5.Enabled = false;
+            //    }
+            //    if (comboBox6.Text == string.Empty)
+            //    {
+            //        comboBox6.Enabled = false;
+            //    }
+            //    if (comboBox7.Text == string.Empty)
+            //    {
+            //        comboBox7.Enabled = false;
+            //    }
+
+            //}
+
+
+            //void Enabler()
+            //{
+            //    comboBox1.Enabled = true;
+            //    comboBox2.Enabled = true;
+            //    comboBox3.Enabled = true;
+            //    comboBox4.Enabled = true;
+            //    comboBox5.Enabled = true;
+            //    comboBox6.Enabled = true;
+            //    comboBox7.Enabled = true;
+            //}
+            #endregion
+
+
+            var specificBookForAuthor = new List<Book>();
+
+            using (var _context = new GuramDBEntities())
+            {
+                _context.Database.CommandTimeout = 200;
+
+                var First = name.Split(' ').First();
+                var Last = name.Remove(0, First.Length).TrimStart();
+
+
+                
+
+
+                specificBookForAuthor = _context.Books.Where(c =>
+                                                    (c.Author.Name.Contains(First) || c.Author.Surname.Contains(Last))
+                                                    && c.Name.Contains(book)
+                                                    && c.Author.Country.Name.Contains(country)
+                                                    && c.Genre.Name.Contains(genre)
+                                                    //&& inputYear == string.Empty ? true : c.Year == year
+                                                    //&& inputStartDate == string.Empty ? true : c.Year >= startDate
+                                                    //&& inputEndDate == string.Empty ? true : c.Year <= endDate
+                                                    ).Select(b => b).ToList();
+
+                
+
+                //specificBookForAuthor = _context.Books.Where(c =>
+                //                                                    (c.Author.Name.Contains(First) || c.Author.Surname.Contains(Last))
+                //                                                    && book == string.Empty ? true : c.Name.Contains(book)
+                //                                                    && country == string.Empty ? true : c.Author.Country.Name.Contains(country)
+                //                                                    && genre == string.Empty ? true : c.Genre.Name.Contains(genre)
+                //                                                    //&& inputYear == string.Empty ? true : c.Year == year
+                //                                                    //&& inputStartDate == string.Empty ? true : c.Year >= startDate
+                //                                                    //&& inputEndDate == string.Empty ? true : c.Year <= endDate
+                //                                                    ).Select(b => b).ToList();
+                
+
+                richTextBox1.Clear();
+
+                foreach (var item in specificBookForAuthor)
+                {
+
+                    richTextBox1.AppendText($"Author :{item.Author.Name} {item.Author.Surname} \nBook : {item.Name}, \nGenre : {item.Genre.Name}, \nCountry : {item.Author.Country.Name} \nYear:{item.Year}  \n----------------------------------------------------\n");
+                }
+
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            richTextBox1.Enabled = true;
+        }
+
+
+        private int MaxAuthorId()
+        {
+            int output = 0;
+            using (GuramDBEntities _ctx = new GuramDBEntities())
+            {
+                var ids = _ctx.Authors.Select(c => c.Id).ToList();
+                var maxId = ids.Count == 0 ? 0 : ids.Max();
+
+
+                output = maxId + 1;
+
+            }
+            return output;
+        }
+
+
+        private int MaxCountryId()
+        {
+            int output = 0;
+            using (GuramDBEntities _ctx = new GuramDBEntities())
+            {
+                var ids = _ctx.Countries.Select(c => c.Id).ToList();
+                var maxId = ids.Count == 0 ? 0 : ids.Max();
+
+
+                output = maxId + 1;
+
+            }
+            return output;
+        }
+
+        private int MaxGenreId()
+        {
+            int output = 0;
+            using (GuramDBEntities _ctx = new GuramDBEntities())
+            {
+                var ids = _ctx.Genres.Select(c => c.Id).ToList();
+                var maxId = ids.Count == 0 ? 0 : ids.Max();
+
+
+                output = maxId + 1;
+
+            }
+            return output;
+        }
+
+        private int MaxBookId()
+        {
+            int output = 0;
+            using (GuramDBEntities _ctx = new GuramDBEntities())
+            {
+                var ids = _ctx.Books.Select(c => c.Id).ToList();
+                var maxId = ids.Count == 0 ? 0 : ids.Max();
+
+
+                output = maxId + 1;
+
+            }
+            return output;
+        }
+
+        //ADD
+        private void ADD_Click(object sender, EventArgs e)
+        {
+            var author = comboBox1.Text;
+            var book = comboBox2.Text;
+            var genre = comboBox3.Text;
+            var country = comboBox4.Text;
+            var year = Int32.Parse(comboBox7.Text);
+
+            int? checkauthor;
+            int? checkbook;
+            int? checkgenre;
+            int? checkcountry;
+
+
+            var First = author.Split(' ').First();
+            var Last = author.Remove(0, First.Length).TrimStart();
+
+            var FirstLast = First + Last;
+
+            using (var _ctx = new GuramDBEntities())
+            {
+                checkauthor = _ctx.Authors.Where(c => (c.Name.ToLower() + c.Surname.ToLower()) == FirstLast.ToLower()).Select(a => a.Id).FirstOrDefault();
+                checkbook = _ctx.Books.Where(c => c.Name.ToLower() == book.ToLower()).Select(a => a.Id).FirstOrDefault();
+                checkgenre = _ctx.Genres.Where(c => c.Name.ToLower() == genre.ToLower()).Select(a => a.Id).FirstOrDefault();
+                checkcountry = _ctx.Countries.Where(c => c.Name.ToLower() == country.ToLower()).Select(a => a.Id).FirstOrDefault();
+            }
+            Country newCountry = new Country();
+
+
+            Random rand = new Random();
+            if (checkcountry == 0)
+            {
+                newCountry = new Country()
+                {
+                    Id = MaxCountryId(),
+                    Name = country
+                };
+
+                using (var _context = new GuramDBEntities())
+                {
+                    _context.Countries.Add(newCountry);
+
+                    _context.SaveChanges();
+                }
+
+                checkcountry = newCountry.Id;
+            }
+
+            Author newAuthor = new Author();
+
+            if (checkauthor == 0)
+            {
+                newAuthor = new Author()
+                {
+                    Id = MaxAuthorId(),
+                    Name = First,
+                    Surname = Last,
+                    CountryId = checkcountry
+                };
+
+                using (var _context = new GuramDBEntities())
+                {
+                    _context.Authors.Add(newAuthor);
+
+                    _context.SaveChanges();
+                }
+
+                checkauthor = newAuthor.Id;
+            }
+
+
+            Genre newGenre = new Genre();
+
+            if (checkgenre == 0)
+            {
+                newGenre = new Genre()
+                {
+                    Id = MaxGenreId(),
+                    Name = genre,
+                };
+
+                using (var _context = new GuramDBEntities())
+                {
+                    _context.Genres.Add(newGenre);
+
+                    _context.SaveChanges();
+                }
+
+                checkgenre = newGenre.Id;
+            }
+
+            Book newBook = new Book();
+
+            if (checkbook == 0)
+            {
+                newBook = new Book()
+                {
+                    Id = MaxBookId(),
+                    Name = book,
+                    GenreId = checkgenre,
+                    AuthorId = checkauthor,
+                    Year = year
+                };
+
+                using (var _context = new GuramDBEntities())
+                {
+                    _context.Books.Add(newBook);
+
+                    _context.SaveChanges();
+                }
+
+                checkbook = newBook.Id;
+            }
+
+
+        }
+
+        //CLEAR
+        private void button2_Click(object sender, EventArgs e)
+        {
+            comboBox1.Text = "";
+            comboBox2.Text = "";
+            comboBox3.Text = "";
+            comboBox4.Text = "";
+            comboBox5.Text = "";
+            comboBox6.Text = "";
+            comboBox7.Text = "";
+        }
+    }
+}
